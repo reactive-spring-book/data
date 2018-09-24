efda54d279f68ce61e9f84fb49077a2555d5ee3b
Josh Long <josh@joshlong.com>
2018-09-23 22:37:06 -0400

    polish

--- src/main/java/rsb/data/mongo/OrderService.java
+++ src/main/java/rsb/data/mongo/OrderService.java
@@ -2,9 +2,6 @@ package rsb.data.mongo;
 
 import org.springframework.stereotype.Service;
 
-/**
- * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
- */
 @Service
 public class OrderService {
 
