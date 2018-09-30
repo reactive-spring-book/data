404115478acd1eb86beda9d091db6df2cb222947
Josh Long <josh@joshlong.com>
2018-09-30 00:44:07 -0700

    polish

--- mongodb/src/test/java/rsb/data/mongodb/OrderServiceTest.java
+++ mongodb/src/test/java/rsb/data/mongodb/OrderServiceTest.java
@@ -1,7 +1,10 @@
 package rsb.data.mongodb;
 
+import com.mongodb.reactivestreams.client.MongoCollection;
 import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
+import org.bson.Document;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,8 +14,11 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.FileCopyUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.io.BufferedReader;
@@ -35,6 +41,9 @@ public class OrderServiceTest {
 	@Autowired
 	private OrderService orderService;
 
+	@Autowired
+	private ReactiveMongoTemplate reactiveMongoTemplate;
+
 	@BeforeClass
 	public static void warn() throws Exception {
 		File here = new File(".");
@@ -51,6 +60,25 @@ public class OrderServiceTest {
 					+ "To test, I usually use a script like this to start up my instance:\n\n"
 					+ contents);
 		}
+	}
+
+	@Before
+	public void configureCollectionsBeforeTests() {
+
+		Mono<Boolean> collectionExists = this.reactiveMongoTemplate
+				.collectionExists(Order.class);
+		Mono<MongoCollection<Document>> createIfMissing = collectionExists.filter(x -> !x)
+				.flatMap(doesNotExist -> this.reactiveMongoTemplate
+						.createCollection(Order.class));
+
+		StepVerifier.create(createIfMissing).verifyComplete();
+
+		/*
+		 * Flux<MongoCollection<Document>> setup = this.reactiveMongoTemplate
+		 * .dropCollection(Order.class)
+		 * .thenMany(this.reactiveMongoTemplate.createCollection(Order.class));
+		 * StepVerifier.create(setup).verifyComplete();
+		 */
 	}
 
 	@Test
