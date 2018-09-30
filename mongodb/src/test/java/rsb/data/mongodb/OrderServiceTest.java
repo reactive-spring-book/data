ddbb584ecd3772f15a358c48bd7da987babc3f43
Josh Long <josh@joshlong.com>
2018-09-30 01:21:04 -0700

    polish

--- mongodb/src/test/java/rsb/data/mongodb/OrderServiceTest.java
+++ mongodb/src/test/java/rsb/data/mongodb/OrderServiceTest.java
@@ -1,9 +1,7 @@
 package rsb.data.mongodb;
 
-import com.mongodb.reactivestreams.client.MongoCollection;
 import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
-import org.bson.Document;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,7 +15,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.FileCopyUtils;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
