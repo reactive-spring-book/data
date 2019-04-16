e941afb640aa3b03df4d482eb000298c28d292fb
Josh Long <josh@joshlong.com>
2019-04-17 06:53:01 +0800

    up

--- mongodb/src/main/java/rsb/data/mongodb/OrderRepository.java
+++ mongodb/src/main/java/rsb/data/mongodb/OrderRepository.java
@@ -3,6 +3,7 @@ package rsb.data.mongodb;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
+//<1>
 interface OrderRepository extends ReactiveCrudRepository<Order, String> {
 
 	Flux<Order> findByProductId(String productId);
