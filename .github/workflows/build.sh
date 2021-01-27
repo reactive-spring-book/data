bfb76c245490d329dd61794c8680908f452fd6c8
Josh Long <josh@joshlong.com>
2021-01-27 15:59:48 -0800

    dbs

---
+++ .github/workflows/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd $GITHUB_WORKSPACE 
+
+ci/bin/setup-mongodb.sh
+ci/bin/setup-postgresql.sh
+
+echo "Starting build"
+mkdir -p $HOME/.m2/
+cp .ci.settings.xml $HOME/.m2/settings.xml
+mvn -e -f $GITHUB_WORKSPACE/pom.xml  verify deploy
+echo "Stopping build" 
