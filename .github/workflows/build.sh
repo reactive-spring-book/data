ce1f6364f464d7cdc9d8c276ad8701ad90bec5a1
Josh Long <josh@joshlong.com>
2021-01-27 16:08:39 -0800

    dbs

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,11 +2,11 @@
 
 cd $GITHUB_WORKSPACE 
 
-sudo apt update && sudo apt upgrade
-sudo apt install curl 
+apt update && sudo apt upgrade
+apt install curl 
 
-ci/bin/setup-mongodb.sh
-ci/bin/setup-postgresql.sh
+./ci/bin/setup-mongodb.sh
+./ci/bin/setup-postgresql.sh
 
 echo "Starting build"
 mkdir -p $HOME/.m2/
