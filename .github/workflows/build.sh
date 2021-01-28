70c2fcfcbb4c4c33061a684db42aca5180783039
Josh Long <josh@joshlong.com>
2021-01-27 16:16:57 -0800

    dbs

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -7,12 +7,12 @@ sudo apt-get -y upgrade
 sudo apt-get -y install libcurl3  
 sudo apt-get -y install libcurl-openssl1.0-dev
 
-./ci/bin/setup-mongodb.sh
-./ci/bin/setup-postgresql.sh
+# ./ci/bin/setup-mongodb.sh
+# ./ci/bin/setup-postgresql.sh
 
 
-echo "Starting build"
-mkdir -p $HOME/.m2/
-cp .ci.settings.xml $HOME/.m2/settings.xml
-mvn -e -f $GITHUB_WORKSPACE/pom.xml  verify deploy
-echo "Stopping build" 
+# echo "Starting build"
+# mkdir -p $HOME/.m2/
+# cp .ci.settings.xml $HOME/.m2/settings.xml
+# mvn -e -f $GITHUB_WORKSPACE/pom.xml  verify deploy
+# echo "Stopping build" 
