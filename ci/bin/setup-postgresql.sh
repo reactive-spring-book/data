138c9415e438a8d65ba3a464cebdce1932ca35ae
Josh Long <josh@joshlong.com>
2021-01-27 16:58:59 -0800

    up

--- ci/bin/setup-postgresql.sh
+++ ci/bin/setup-postgresql.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+
+sudo apt install postgresql postgresql-contrib
+# sudo -i -u postgres
+# sudo -u postgres psql
 psql -c 'CREATE DATABASE orders;' -U postgres
 psql -c 'create table customer( id  serial primary key, email varchar(255) not null);' -U postgres orders
