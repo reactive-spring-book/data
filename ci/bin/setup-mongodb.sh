37928bd5ac6a3cc27c3a1de27a0ef7c7f5301307
Josh Long <josh@joshlong.com>
2021-01-27 18:18:35 -0800

    up

--- ci/bin/setup-mongodb.sh
+++ ci/bin/setup-mongodb.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
-MONGO_VERSION=4.0.0
+MONGO_VERSION=4.4.3
 mkdir -p downloads
 mkdir -p var/db var/log
+
+# https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.4.3.tgz
+
 if [[ ! -d downloads/mongodb-linux-x86_64-ubuntu1604-${MONGO_VERSION} ]] ; then
     cd downloads \
-        && wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-${MONGO_VERSION}.tgz \
+        && wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-${MONGO_VERSION}.tgz \
         && tar xzf mongodb-linux-x86_64-ubuntu1604-${MONGO_VERSION}.tgz \
         && cd ..;
 fi
-downloads/mongodb-linux-x86_64-ubuntu1604-${MONGO_VERSION}/bin/mongod --version
-downloads/mongodb-linux-x86_64-ubuntu1604-${MONGO_VERSION}/bin/mongod --dbpath var/db --replSet rs0 --fork --logpath var/log/mongod.log
+
+# ubuntu-18.04 
+# is what github actions uses 
+ 
+MONGO_HOME=$(pwd)/downloads/mongodb-linux-x86_64-ubuntu1804-${MONGO_VERSION}
+${MONGO_HOME}/bin/mongod --version
+${MONGO_HOME}/bin/mongod --dbpath var/db --replSet rs0 --fork --logpath var/log/mongod.log
 sleep 10
 
-downloads/mongodb-linux-x86_64-ubuntu1604-${MONGO_VERSION}/bin/mongo --eval "rs.initiate({_id: 'rs0', members:[{_id: 0, host: '127.0.0.1:27017'}]});"
+${MONGO_HOME}/bin/mongo --eval "rs.initiate({_id: 'rs0', members:[{_id: 0, host: '127.0.0.1:27017'}]});"
 sleep 15
 
