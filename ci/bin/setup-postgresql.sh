a4efe885484e68f58b8c4b1e54ee96584e28ab72
Josh Long <josh@joshlong.com>
2021-01-27 17:06:05 -0800

    postgresql

--- ci/bin/setup-postgresql.sh
+++ ci/bin/setup-postgresql.sh
@@ -8,5 +8,4 @@ psql -U postgres -c "select * from customer "
 # sudo -u postgres psql
 # psql -c  "CREATE DATABASE orders;" -U postgres
 # psql -c  "CREATE user orders with password  'orders'; grant all  " -U postgres
-
 # psql -c 'create table customer( id  serial primary key, email varchar(255) not null);' -U postgres orders
