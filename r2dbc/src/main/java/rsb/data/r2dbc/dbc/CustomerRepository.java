7d58b76bf4d8b97e49c7107c173c83e23ec92b97
Josh Long <josh@joshlong.com>
2022-01-07 18:10:44 -0800

    customer repository

--- r2dbc/src/main/java/rsb/data/r2dbc/dbc/CustomerRepository.java
+++ r2dbc/src/main/java/rsb/data/r2dbc/dbc/CustomerRepository.java
@@ -14,58 +14,58 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CustomerRepository implements SimpleCustomerRepository {
 
-    private final DatabaseClient databaseClient;
+	private final DatabaseClient databaseClient;
 
-    private Customer map(Map<String, Object> row) {
-        return new Customer((Integer) row.get("id"), (String) row.get("email"));
-    }
+	private Customer map(Map<String, Object> row) {
+		return new Customer((Integer) row.get("id"), (String) row.get("email"));
+	}
 
-    // <1>
-    @Override
-    public Flux<Customer> findAll() {
-        return databaseClient.sql("select * from customer").fetch().all().as(rows -> rows.map(this::map));
-    }
+	// <1>
+	@Override
+	public Flux<Customer> findAll() {
+		return databaseClient.sql("select * from customer").fetch().all().as(rows -> rows.map(this::map));
+	}
 
-    // <2>
-    @Override
-    public Mono<Customer> save(Customer c) {
-        return this.databaseClient //
-                .sql("insert into customer( email ) values( $1  )") //
-                .bind("$1", c.email()) //
-                .filter((stmt, ef) -> stmt.returnGeneratedValues("id").execute()).fetch().first()
-                .flatMap(row -> findById((Integer) row.get("id")));
-    }
+	// <2>
+	@Override
+	public Mono<Customer> save(Customer c) {
+		return this.databaseClient //
+				.sql("insert into customer( email ) values( $1  )") //
+				.bind("$1", c.email()) //
+				.filter((stmt, ef) -> stmt.returnGeneratedValues("id").execute()).fetch().first()
+				.flatMap(row -> findById((Integer) row.get("id")));
+	}
 
-    @Override
-    public Mono<Customer> update(Customer c) {
-        return databaseClient //
-                .sql(" update customer set email = $1 where id = $2 ") //
-                .bind("$1", c.email()) //
-                .bind("$2", c.id()) //
-                .fetch() //
-                .first() //
-                .switchIfEmpty(Mono.empty()).then(findById(c.id()));
-    }
+	@Override
+	public Mono<Customer> update(Customer c) {
+		return databaseClient //
+				.sql(" update customer set email = $1 where id = $2 ") //
+				.bind("$1", c.email()) //
+				.bind("$2", c.id()) //
+				.fetch() //
+				.first() //
+				.switchIfEmpty(Mono.empty()).then(findById(c.id()));
+	}
 
-    @Override
-    public Mono<Customer> findById(Integer id) {
-        return this.databaseClient //
-                .sql("select * from customer where id = $1 ") //
-                .bind("$1", id) //
-                .fetch() //
-                .first() //
-                .map(map -> new Customer(//
-                        (Integer) map.get("id"), //
-                        (String) map.get("email") //
-                ));
-    }
+	@Override
+	public Mono<Customer> findById(Integer id) {
+		return this.databaseClient //
+				.sql("select * from customer where id = $1 ") //
+				.bind("$1", id) //
+				.fetch() //
+				.first() //
+				.map(map -> new Customer(//
+						(Integer) map.get("id"), //
+						(String) map.get("email") //
+				));
+	}
 
-    // <3>
-    @Override
-    public Mono<Void> deleteById(Integer id) {
-        return this.databaseClient.sql("DELETE FROM customer where id = $1") //
-                .bind("$1", id) //
-                .fetch().rowsUpdated().then();
-    }
+	// <3>
+	@Override
+	public Mono<Void> deleteById(Integer id) {
+		return this.databaseClient.sql("DELETE FROM customer where id = $1") //
+				.bind("$1", id) //
+				.fetch().rowsUpdated().then();
+	}
 
 }
