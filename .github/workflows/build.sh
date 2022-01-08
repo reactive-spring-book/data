f560723b1ad9a3fe763b1c3b8ca26f3f17f8a340
Josh Long <josh@joshlong.com>
2022-01-07 19:41:43 -0800

    ci

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,7 +4,6 @@ cd $GITHUB_WORKSPACE
 echo "Starting build"
 mkdir -p $HOME/.m2/
 cp $GITHUB_WORKSPACE/.ci.settings.xml $HOME/.m2/settings.xml
-
-mvn -e -f $GITHUB_WORKSPACE/pom.xml verify package
+mvn -e -f $GITHUB_WORKSPACE/pom.xml clean package
 echo "Stopping build"
 
